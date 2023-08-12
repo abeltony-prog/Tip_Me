f273d1216a19c995ffb4e4bb6ef12ad361471495
abeltony-prog <abeltony03@gmail.com>
2023-08-12 17:20:58 +0200

    tape me design

--- src/components/Survey/form.tsx
+++ src/components/Survey/form.tsx
@@ -1,85 +1,188 @@
 import React from "react";
 
-export default function SurveyForm(){
-    return(
-        <>
-           <div className="bg-white border rounded-lg px-8 py-6 mx-auto my-8 max-w-2xl">
-
-    <h2 className="text-2xl font-medium mb-4">Survey</h2>
-    <form>
-        <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 font-medium mb-2">Name</label>
-            <input type="text" id="name" name="name"
-                className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" required />
-        </div>
-        <div className="mb-4">
-            <label htmlFor="age" className="block text-gray-700 font-medium mb-2">Age</label>
-            <input type="number" id="age" name="age"
-                className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" required />
-        </div>
-        <div className="mb-4">
-            <label htmlFor="gender" className="block text-gray-700 font-medium mb-2">Gender</label>
-            <select id="gender" name="gender"
-                className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" required>
-                <option value="">Select gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
+export default function SurveyForm() {
+  return (
+    <>
+      <div className="mx-auto my-8 max-w-2xl rounded-lg border bg-white px-8 py-6">
+        <h2 className="mb-4 text-2xl font-medium">Survey</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="mb-2 block font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full rounded-lg border border-gray-400 p-2 focus:border-blue-400 focus:outline-none"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="age"
+              className="mb-2 block font-medium text-gray-700"
+            >
+              Age
+            </label>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              className="w-full rounded-lg border border-gray-400 p-2 focus:border-blue-400 focus:outline-none"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="gender"
+              className="mb-2 block font-medium text-gray-700"
+            >
+              Gender
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              className="w-full rounded-lg border border-gray-400 p-2 focus:border-blue-400 focus:outline-none"
+              required
+            >
+              <option value="">Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
             </select>
-        </div>
-        <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">What is your favorite color?</label>
-            <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2" />Red
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2" />Blue
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2" />Green
-                    </label>
-                </div>
+          </div>
+          <div className="mb-4">
+            <label className="mb-2 block font-medium text-gray-700">
+              What is your favorite color?
+            </label>
+            <div className="-mx-2 flex flex-wrap">
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="color-red"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="radio"
+                    id="color-red"
+                    name="color"
+                    value="red"
+                    className="mr-2"
+                  />
+                  Red
+                </label>
+              </div>
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="color-blue"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="radio"
+                    id="color-blue"
+                    name="color"
+                    value="blue"
+                    className="mr-2"
+                  />
+                  Blue
+                </label>
+              </div>
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="color-green"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="radio"
+                    id="color-green"
+                    name="color"
+                    value="green"
+                    className="mr-2"
+                  />
+                  Green
+                </label>
+              </div>
             </div>
-        </div>
-        <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">What is your favorite animal?</label>
-            <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
-                    <label htmlFor="animal-cat" className="block text-gray-700 font-medium mb-2">
-                        <input type="checkbox" id="animal-cat" name="animal[]" value="cat" className="mr-2" />Cat
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="animal-dog" className="block text-gray-700 font-medium mb-2">
-                        <input type="checkbox" id="animal-dog" name="animal[]" value="dog"
-                            className="mr-2" />Dog
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="animal-bird" className="block text-gray-700 font-medium mb-2">
-                        <input type="checkbox" id="animal-bird" name="animal[]" value="bird" className="mr-2" />Bird
-                    </label>
-                </div>
+          </div>
+          <div className="mb-4">
+            <label className="mb-2 block font-medium text-gray-700">
+              What is your favorite animal?
+            </label>
+            <div className="-mx-2 flex flex-wrap">
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="animal-cat"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="checkbox"
+                    id="animal-cat"
+                    name="animal[]"
+                    value="cat"
+                    className="mr-2"
+                  />
+                  Cat
+                </label>
+              </div>
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="animal-dog"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="checkbox"
+                    id="animal-dog"
+                    name="animal[]"
+                    value="dog"
+                    className="mr-2"
+                  />
+                  Dog
+                </label>
+              </div>
+              <div className="w-1/3 px-2">
+                <label
+                  htmlFor="animal-bird"
+                  className="mb-2 block font-medium text-gray-700"
+                >
+                  <input
+                    type="checkbox"
+                    id="animal-bird"
+                    name="animal[]"
+                    value="bird"
+                    className="mr-2"
+                  />
+                  Bird
+                </label>
+              </div>
             </div>
-        </div>
-        <div className="mb-4">
-            <label htmlFor="message" className="block text-gray-700 font-medium mb-2">Message</label>
-            <textarea id="message" name="message"
-                className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" rows={5}></textarea>
-        </div>
-        <div>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Submit</button>
-        </div>
-
-    </form>
-    </div>
-
-        </>
-    )
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="mb-2 block font-medium text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full rounded-lg border border-gray-400 p-2 focus:border-blue-400 focus:outline-none"
+              rows={5}
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/components/mainbody.tsx
+++ src/components/mainbody.tsx
@@ -1,62 +1,69 @@
 import React from "react";
 
-export default function MainBody(){
-    return(
-        <>
-     
-     <div className="flex flex-col md:flex-row space-y-4 md:mt-[233px] md:ml-[460px] md:space-y-0 md:space-x-4">
-     <div className="w-[433px] h-[433px] flex justify-center items-center">
-    <div className="absolute w-80 h-96 ml-[50px] rotate-6 rounded-2xl bg-gray-400 transform scale-200"></div>
+export default function MainBody() {
+  return (
+    <>
+      <div className="flex flex-col space-y-4 md:mt-[233px] md:ml-[460px] md:flex-row md:space-y-0 md:space-x-4">
+        <div className="flex h-[433px] w-[433px] items-center justify-center">
+          <div className="scale-200 absolute ml-[50px] h-96 w-80 rotate-6 transform rounded-2xl bg-gray-400"></div>
 
-    <div className="w-80 h-96 rotate-6 space-y-6 rounded-2xl bg-gray-100 p-6 transition duration-300 hover:rotate-0 transform scale-200">
-        <div className="flex justify-end">
-            <div className="h-4 w-4 rounded-full bg-gray-900"></div>
-        </div>
+          <div className="scale-200 h-96 w-80 rotate-6 transform space-y-6 rounded-2xl bg-gray-100 p-6 transition duration-300 hover:rotate-0">
+            <div className="flex justify-end">
+              <div className="h-4 w-4 rounded-full bg-gray-900"></div>
+            </div>
 
-        <header className="text-center text-xl font-extrabold text-gray-600">2021.09.01</header>
+            <header className="text-center text-xl font-extrabold text-gray-600">
+              2021.09.01
+            </header>
 
-        <div>
-            <p className="text-center text-5xl font-extrabold text-gray-900">Tip (Me)</p>
-            <p className="text-center text-4xl font-extrabold text-[#FE5401]">waiter / chef
-</p>
-        </div>
+            <div>
+              <p className="text-center text-5xl font-extrabold text-gray-900">
+                Tip (Me)
+              </p>
+              <p className="text-center text-4xl font-extrabold text-[#FE5401]">
+                waiter / chef
+              </p>
+            </div>
 
-        <footer className="mb-10 flex justify-center">
-            <button className="flex items-baseline gap-2 rounded-lg bg-[#FE5401] px-4 py-2.5 text-xl font-bold text-white hover:bg-[#FF7308]">
+            <footer className="mb-10 flex justify-center">
+              <button className="flex items-baseline gap-2 rounded-lg bg-[#FE5401] px-4 py-2.5 text-xl font-bold text-white hover:bg-[#FF7308]">
                 <span>Start</span>
                 <i className="fas fa-hand-peace text-xl"></i>
-            </button>
-        </footer>
-    </div>
-</div>
+              </button>
+            </footer>
+          </div>
+        </div>
 
+        <div className="h-[433px] w-[433px] place-items-center sm:ml-[134px]">
+          <div className="scale-200 absolute ml-[25px] h-96 w-80 rotate-6 transform rounded-2xl bg-gray-400"></div>
 
-    <div className="w-[433px] place-items-center sm:ml-[134px] h-[433px]">
-    <div className="absolute w-80 h-96 ml-[25px] rotate-6 rounded-2xl bg-gray-400 transform scale-200"></div>
+          <div className="scale-200 h-96 w-80 rotate-6 transform space-y-6 rounded-2xl bg-gray-100 p-6 transition duration-300 hover:rotate-0">
+            <div className="flex justify-end">
+              <div className="h-4 w-4 rounded-full bg-gray-900"></div>
+            </div>
 
-<div className="w-80 h-96 rotate-6 space-y-6 rounded-2xl bg-gray-100 p-6 transition duration-300 hover:rotate-0 transform scale-200">
-    <div className="flex justify-end">
-        <div className="h-4 w-4 rounded-full bg-gray-900"></div>
-    </div>
+            <header className="text-center text-xl font-extrabold text-gray-600">
+              2021.09.01
+            </header>
 
-    <header className="text-center text-xl font-extrabold text-gray-600">2021.09.01</header>
+            <div>
+              <p className="text-center text-5xl font-extrabold text-gray-900">
+                Rate Us (Survey)
+              </p>
+              <p className="text-center text-4xl font-extrabold text-[#FE5401]">
+                Restaurant
+              </p>
+            </div>
 
-    <div>
-        <p className="text-center text-5xl font-extrabold text-gray-900">Rate Us (Survey)</p>
-        <p className="text-center text-4xl font-extrabold text-[#FE5401]">Restaurant</p>
-    </div>
-
-    <footer className="mb-10 flex justify-center">
-        <button className="flex items-baseline gap-2 rounded-lg bg-[#FE5401] px-4 py-2.5 text-xl font-bold text-white hover:bg-[#FF7308]">
-            <span>Start</span>
-            <i className="fas fa-hand-peace text-xl"></i>
-        </button>
-    </footer>
-</div>
-    </div>
-</div>
-    
-
-        </>
-    )
+            <footer className="mb-10 flex justify-center">
+              <button className="flex items-baseline gap-2 rounded-lg bg-[#FE5401] px-4 py-2.5 text-xl font-bold text-white hover:bg-[#FF7308]">
+                <span>Start</span>
+                <i className="fas fa-hand-peace text-xl"></i>
+              </button>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
